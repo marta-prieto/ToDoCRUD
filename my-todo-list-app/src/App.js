75ff2e75029290514f2890f14db361470ce66253
Marta Prieto <martaprietoluque@gmail.com>
2024-10-13 01:25:05 +0200

    Add style files with icons and css smooth

--- my-todo-list-app/src/App.js
+++ my-todo-list-app/src/App.js
@@ -1,4 +1,7 @@
-//import logo from './logo.svg';
+import logo from './images/logo-img.svg';
+import menu from './images/menu-img.svg';
+import search from './images/search-img.svg';
+import cart from './images/cart-img.svg';
 import './App.scss';
 
 function App() {
@@ -6,21 +9,28 @@ function App() {
     <div className="App">
       <header>
     <nav aria-label="Menú principal">
-      <div className="logo">
-        <a href="https://www.tiendanimal.es/" aria-label="Ir al inicio">
-        <img src="https://www.tiendanimal.es/on/demandware.static/Sites-TiendanimalES-Site/-/default/dw330b2f7b/images/logo/tiendanimal-mobile.svg" className="navbar-header_logo" alt="logo" />
-        </a>
+      <div className="header__images-container">
+      <img src= { menu } className="navbar__header-logo menu" alt="menu icono"/>
+      <img src= { search } className="navbar__header-logo search" alt="buscador icono"/>
+      <img src= { logo } className="navbar__header-logo logo" alt="logo icono"/>
+      <img src= { cart } className="navbar__header-logo cart" alt="carrito compra icono"/>
+
+        {/* <a href="https://www.tiendanimal.es/" aria-label="Ir al inicio">
+         <img src="https://www.tiendanimal.es/on/demandware.static/Sites-TiendanimalES-Site/-/default/dw330b2f7b/images/logo/tiendanimal-mobile.svg" className="navbar-header_logo" alt="logo" />
+      
+        </a> */}
       </div>
-     
-    
-    </nav>
-    <ul className="menu">
-        <li><a href="#datos">Mis datos</a></li>
-        <li><a href="#tareas">Mis tareas</a></li>
-        <li><a href="#devoluciones">Mis devoluciones</a></li>
-        <li><a href="#comunicaciones">Mis comunicaciones</a></li>
-        <li><a href="#amigos">Mis mejores amigos</a></li>
+      <div className='menu__container-list'>
+      <ul className="menu">
+        <li className='item__menu'>Mis datos</li>
+        <li className='item__menu'>Mis tareas</li>
+        <li className='item__menu'>Mis devoluciones</li>
+        <li className='item__menu'>Mis comunicaciones</li>
+        <li className='item__menu'>Mis mejores amigos</li>
       </ul>
+    </div>
+    </nav>
+  
   </header>
 
   <main>
